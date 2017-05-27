10bcff9c6cd21c5b40a3baef01d7d847f890879c
FreshFlz <saba.f88@gmail.com>
2017-05-27 23:18:09 +0430

    Add files via upload

---
+++ MultiThreadedSort/src/saba/sorting/BubbleSortPanel.java
@@ -0,0 +1,50 @@
+package saba.sorting;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BubbleSortPanel extends SortPanel {
+	private static final long serialVersionUID = 1L;
+	private int redColumn = -1;
+	private int greenColumn = -1;
+	
+	public BubbleSortPanel(String name, int sleepTime, int width, int height) {
+		super(name, sleepTime, width, height);
+	}
+
+	@Override
+	public void reset() {
+		redColumn = -1;
+		greenColumn = -1;		
+	}
+
+	@Override
+	public void run() {
+		try {
+			boolean needNextPass = true;
+			for (int k = 1; k < list.length && needNextPass; k++) {
+				needNextPass = false;
+				for (int i = 0; i < list.length - k; i++) {
+					redColumn = i;
+					repaint();
+					Thread.sleep(4 * sleepTime);
+					if (list[i] > list[i + 1]) {
+						redColumn = i + 1;
+						int temp = list[i];
+						list[i] = list[i + 1];
+						list[i + 1] = temp;
+						repaint();
+						Thread.sleep(4 * sleepTime);
+						needNextPass = true;
+					}
+				}
+				greenColumn = size - k;
+			}
+			greenColumn = 0;
+			redColumn = -1;
+		} catch (InterruptedException e) {
+		}
+		repaint();
+	}
+
+}
---
+++ MultiThreadedSort/src/saba/sorting/InsertionSortPanel.java
@@ -0,0 +1,47 @@
+package saba.sorting;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class InsertionSortPanel extends SortPanel {
+	private static final long serialVersionUID = 1L;
+	private int redColumn = -1;
+	private int greenColumn = -1;
+	
+	public InsertionSortPanel(String name, int sleepTime, int width, int height) {
+		super(name, sleepTime, width, height);
+	}
+
+	@Override
+	public void reset() {
+		redColumn = -1;
+		greenColumn = -1;		
+	}
+
+	@Override
+	public void run() {
+		try {
+			for (int i = 1; i < list.length; i++) {
+				greenColumn = i;
+				redColumn = greenColumn;
+				int k;
+				for (k = i - 1; k >= 0 && list[k] > list[k + 1]; k--) {
+					Thread.sleep(3 * sleepTime);
+					repaint();
+					redColumn = k + 1;
+					repaint();
+					Thread.sleep(4 * sleepTime);
+					int tmp = list[k + 1]; 
+					list[k + 1] = list[k];
+					list[k] = tmp;
+				}
+				redColumn = k + 1;
+				repaint();
+			}
+			redColumn = -1;
+		} catch (InterruptedException e) {
+		}
+		repaint();
+	}
+
+}
